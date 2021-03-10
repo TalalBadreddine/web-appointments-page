7171fab79c652eac3a8ecfbfefea1ba83b8bee8c
sara <sara.skandar@hotmail.com>
2021-03-10 22:17:28 +0200

    appointments app

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,17 @@
-import * as firebase from "firebase";  
-      
+import firebase from 'firebase/app';
+require('firebase/auth');
+require('firebase/database');
+
 var firebaseConfig = {
-    apiKey: "AIzaSyAyp2ZMaL1iuYv_PyZ31ErMSseS4G_NUiU",
-    authDomain: "appointments-crud.firebaseapp.com",
-    projectId: "appointments-crud",
-    storageBucket: "appointments-crud.appspot.com",
-    messagingSenderId: "91707071342",
-    appId: "1:91707071342:web:a94add3e3c3335dfd53f7f",
-    measurementId: "G-PSD7TD14TH"
-}; 
-    
-var fireDb = firebase.initializeApp(firebaseConfig);  
-    
-export default fireDb.database().ref();  
+    apiKey: "AIzaSyBzSB-I0oqQy_PS5vnsCO21h9l3mNlHPaA",
+    authDomain: "appointment-crud.firebaseapp.com",
+    databaseURL: "https://appointment-crud-default-rtdb.firebaseio.com",
+    projectId: "appointment-crud",
+    storageBucket: "appointment-crud.appspot.com",
+    messagingSenderId: "240486821887",
+    appId: "1:240486821887:web:83c134dc4f1f8d6ba441ca"
+  };
+  // Initialize Firebase
+  var fireDb = firebase.initializeApp(firebaseConfig);
+
+  export default fireDb.database().ref();
